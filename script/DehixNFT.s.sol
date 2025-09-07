f3c3c6971321e0dc12686dcc1eaf19effaeb8a4b
Yash Kumar <yashkm194@gmail.ccom>
2025-09-07 21:39:04 +0530

    Error fix

---
+++ script/DehixNFT.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {DehixNFT} from "../src/DehixNFT.sol";
+
+contract DehixNFTScript is Script {
+    // Counter public counter;
+    // function setUp() public {}
+    // function run() public {
+    //     vm.startBroadcast();
+    //     counter = new Counter();
+    //     vm.stopBroadcast();
+    // }
+}
